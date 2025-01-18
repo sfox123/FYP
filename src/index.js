f22500cd3c6c3dc00e9efc0caaf5c98e7376b094
sfox123 <shagyfox133@gmail.com>
2025-01-18 09:44:14 +0530

    env

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 import AnimationScreen from './misc/AnimationScreen';
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
